3687b9176c4058e511e66b4b06b023c55e0e15e8
Abrsparrow <abrahamalemayehu38@gmail.com>
2023-07-04 08:49:50 -0700

    small fix

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,3 +1,5 @@
-#!/usr/bin/python3
-def copy_list(l):
-    return list(l)
+#!/usr/bin3
+def copy_list(original_list):
+       Returns a new list that is a copy of the original list.
+    """
+    return list(original_list)
